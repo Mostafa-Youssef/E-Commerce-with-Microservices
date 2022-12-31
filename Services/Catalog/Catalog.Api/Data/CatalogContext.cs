078aa606800a4c48012f08d00e9e8f6fa7d43f0c
Mostafa AbdelMonaem <most.monaem@gmail.com>
2022-12-31 15:34:10 +0200

    Initial commit

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration) 
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DataBaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DataBaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DataBaseSettings:CollectionName"));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static async Task SeedData(IMongoCollection<Product> productCollection)
+        {
+            if (!productCollection.Find(x => true).Any())
+            {
+                await productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
+
+            var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+            return products;
+        }
+    }
+}
