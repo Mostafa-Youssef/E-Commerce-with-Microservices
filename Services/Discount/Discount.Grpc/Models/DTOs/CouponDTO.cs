ba8e3aaff8c69fcb10485749f0d50f4de28411bb
Mostafa AbdelMonaem <most.monaem@gmail.com>
2023-01-21 18:55:53 +0200

    [Add] Discount Grpc Service

---
+++ Services/Discount/Discount.Grpc/Models/DTOs/CouponDTO.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Discount.Grpc.Models.DTO
+{
+    public class CouponDTO
+    {
+        [Required(ErrorMessage = "id is required")]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string? Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.Grpc.Models.DTO;
+using Discount.Grpc.Models.Entities;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
