82456fe5ab4ee547f88e8001d200a90ede72096c
Mostafa <most.monaem@gmail.com>
2023-08-30 17:51:56 +0300

    add services and http client in api gateway/aggregator

---
+++ ApiGateWays/Shopping.Aggregator/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Shopping.Aggregator.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> ReadContentAs<T>(this HttpResponseMessage message)
+        {
+            if (!message.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"Something went wrong calling the api: {message.ReasonPhrase}");
+            }
+
+            var dataAsString = await message.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+    }
+}
---
+++ ApiGateWays/Shopping.Aggregator/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<BasketItemExtendedModel> Item { get; set; } = new List<BasketItemExtendedModel>();
+
+    }
+}
