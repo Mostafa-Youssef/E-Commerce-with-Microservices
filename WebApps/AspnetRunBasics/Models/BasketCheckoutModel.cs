7632001be8e89e81bac431d36fbd8affa5f98f26
Mostafa <most.monaem@gmail.com>
2023-09-13 18:03:42 +0300

    the end of the course with ui

---
+++ WebApps/AspnetRunBasics/Models/BasketCheckoutModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspnetRunBasics.Models
+{
+    public class BasketCheckoutModel
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int DeliveryMethodId { get; set; }
+        public AddressDto Addres { get; set; }
+        public PaymentDto payment { get; set; }
+    }
+}
---
+++ WebApps/AspnetRunBasics/Models/OrderResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspnetRunBasics.Models
+{
+    public class OrderResponseModel
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public AddressDto Addres { get; set; }
+        public PaymentDto payment { get; set; }
+    }
+}
